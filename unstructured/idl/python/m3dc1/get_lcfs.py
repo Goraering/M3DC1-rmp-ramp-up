785f43b19860ca9a59c512d550b9c9b80c026688
Seahyun Jo <50688627+Goraering@users.noreply.github.com>
2026-04-24 14:12:40 +0900

    edit comments

--- unstructured/idl/python/m3dc1/get_lcfs.py
+++ unstructured/idl/python/m3dc1/get_lcfs.py
@@ -53,11 +53,11 @@ def get_lcfs(psi=None, x=None, z=None, *, psival=None, axis=None, filename="C1.h
             if not cs.collections or not cs.collections[0].get_paths():
                 return np.zeros((2, 0), dtype=float)
             
-            # 모든 path(곡선)를 순회
+            # loop all curves
             for path in cs.collections[0].get_paths():
                 vertices = path.vertices
                 all_x.extend(vertices[:, 0])
-                all_x.append(np.nan) # 곡선 구분을 위해 nan 삽입
+                all_x.append(np.nan)
                 all_y.extend(vertices[:, 1])
                 all_y.append(np.nan)
                 
@@ -67,18 +67,15 @@ def get_lcfs(psi=None, x=None, z=None, *, psival=None, axis=None, filename="C1.h
             if not paths or not paths[0].to_polygons():
                 return np.zeros((2, 0), dtype=float)
             
-            # 분리된 모든 다각형(곡선)을 순회
+            # loop all curves
             for poly in paths[0].to_polygons():
                 all_x.extend(poly[:, 0])
-                all_x.append(np.nan) # 곡선 구분을 위해 nan 삽입
+                all_x.append(np.nan)
                 all_y.extend(poly[:, 1])
                 all_y.append(np.nan)
         
-        # 추출된 데이터가 없는 경우 안전망
         if not all_x:
             return np.zeros((2, 0), dtype=float)
-            
-        # 마지막에 불필요하게 들어간 nan을 제거하고 (2, N) 형태로 반환
         return np.array([all_x[:-1], all_y[:-1]], dtype=float)
 
     finally:
